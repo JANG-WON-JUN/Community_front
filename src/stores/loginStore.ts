0addcbdd8106aa487cd88590346de43fb3becd81
w32807 <w32807@nate.com>
2023-02-23 16:01:43 +0900

    first commit

---
+++ src/stores/loginStore.ts
@@ -0,0 +1,30 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import Member from "../components/models/member"
+
+const loginStore = defineStore({
+  id: 'login',
+  state: () => {
+    return {
+      isAuthed: ref(false),
+      member: ref(new Member("", ""))
+    }
+  },
+
+  // 주의 : getter는 변수명과 동일하게 사용하면 안된다.
+  getters:{
+    // isAuthed: (state) => state.isAuthed, -> not working
+    // member: (state) => state.member, -> not working
+    isAuthedMember: (state) => state.isAuthed,
+    getMember: (state) => state.member,
+  },
+  // this.변수명으로 값을 가져올 때는 getter를 통해 값을 가져오는 듯?
+  actions: {
+    logout(): void{
+      this.isAuthed = false;
+      this.member = new Member("", "");
+    }
+  }
+})
+
+export default loginStore

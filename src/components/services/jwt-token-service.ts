d80dec7bdd00b5def7a8aaef49bad1f287e26125
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-23 16:20:46 +0900

    add class related with jwt

---
+++ src/components/services/jwt-token-service.ts
@@ -0,0 +1,23 @@
+import axios from "../common/axios-config";
+import JwtToken from "../models/jwt-token";
+
+class JwtRefreshService{
+    constructor() {}
+
+    refresh(jwtToken: JwtToken){
+        jwtToken = jwtToken || new JwtToken("", "");
+        axios.post('/refresh/refresh', jwtToken)
+        .then(function(response){
+
+            localStorage.setItem('accessToken', response.data.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.data.refreshToken);
+            // vue-router에서 useRouter를 import 후 화면이동하기.
+            //router.push({name: "home"});
+        })
+        .catch(function(error){
+
+        });
+    }
+}
+
+export default JwtRefreshService

26473c8198f9c76ba05287ee47e2201f5198716f
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-02-23 16:21:03 +0900

    add proxy to solve CORS

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+        // vite.config.js에서 proxy를 두어 특정 url에 매핑할 수 있다.
+        // 프록시를 두어 CORS를 해결한다.
+        // https://vitejs.dev/config/server-options.html 참고
+        "^/api/.*": {
+            target: "http://localhost:8080",
+            changeOrigin: true
+        },
+    },
+},
 });
